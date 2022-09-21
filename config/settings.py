29f2fad05cf8e3e014369126ccc209ec93439ea1
MattDWalsh <matthew.david.walsh@gmail.com>
2022-09-21 12:37:04 -0700

    adjusted static settings

--- config/settings.py
+++ config/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 if DEBUG:
     STATIC_URL = 'static/'
--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.static import serve
 from django.conf import settings
+from django.conf.urls.static import static
 
 # from . import views
 
@@ -31,4 +32,4 @@ urlpatterns = [
     path('', include('trails.urls')),
     re_path(r'^uploads/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
     # path('errortest', views.errortest)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
